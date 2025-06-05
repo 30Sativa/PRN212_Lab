6e27b692dbeb0d2f0067f3afe6fe636d5dd04903
30Sativa <thanhrrgame@gmail.com>
2025-06-05 11:41:33 +0700

    add

--- ProductManagementDemo/Repositories/CategoryRepository.cs
+++ ProductManagementDemo/Repositories/CategoryRepository.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessObjects;
+using DataAccessObjects;
 
 namespace Repositories
 {
-    internal class CategoryRepository
+    public class CategoryRepository : ICatergoryRepository
     {
+        public List<Category> GetCategories() => CategoryDAO.GetCategories();
     }
 }
